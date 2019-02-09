d5ef696563a0c9eccf83fa93e4b44f7fbb5f864c
Ben Botvinick <benbotvinick@gmail.com>
2019-02-08 21:00:00 -0500

    imports

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,11 @@
 import 'bootstrap';
 import $ from 'jquery';
+import TextService from './text_service';
 
 $(() => {
 	chrome.tabs.getSelected((tab) => {
 		const currentURL = new URL(tab.url);
 
-		console.log(currentURL)
+		console.log(currentURL);
 	});
 });
